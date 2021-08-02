71082fdbe969f75f2a4113bf70d7e66fbe802f01
jonathan bleibdrey <jonbleibdrey@aol.com>
2021-08-02 12:18:06 -0400

    first init

---
+++ area_calc.py
@@ -0,0 +1,9 @@
+# name = input("Tell me your name, punk: ")
+# age = input("...your age?: ")
+# print("my name is",name,"and i am ",age)
+
+# calc the are of a circle
+
+radius = input("Enter the radius of your circle (meter): ") 
+area = 3.142 * int(radius)**2
+print("the area of your circle is:", area)
