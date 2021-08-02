a75bec00215142762339468443757ec88ea8f176
jonathan bleibdrey <jonbleibdrey@aol.com>
2021-08-02 15:04:13 -0400

    added if else statment

---
+++ lessons/if_elif.py
@@ -0,0 +1,20 @@
+#OPERATORS
+#<,>,==, !=, => =<
+
+# age = int(input("enter your age: "))
+
+# if age < 10:
+#     #code block
+#     print("you are young strange one")
+# elif age < 40:
+#     print(" you are very old!")
+# else:
+#     print("old as hell!")
+
+
+meaty = input("do you eat meat? (y/n):")
+
+if meaty != "y":
+    print("you are a meat eater!")
+else:
+    print("you are vegetarian")
