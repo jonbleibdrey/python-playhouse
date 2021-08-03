d892fa746a3c567e3ac15d9b56cdea7392879cb9
jonathan bleibdrey <jonbleibdrey@aol.com>
2021-08-03 11:43:38 -0400

    added while loop

--- lessons/loops.py
+++ lessons/loops.py
@@ -1,4 +1,17 @@
-ninjas = ['ryu', 'crystal', 'yoshi','ken']
+# ninjas = ['ryu', 'crystal', 'yoshi','ken']
+# while loop will run as long as that condint is true!
+age = 25
+num = 0
+
+while num <= age:
+    if num == 0:
+        num += 1
+        continue
+    if num % 2 == 0:
+        print(num)
+    if num > 10:
+        break
+    num += 1
 
 # for ninja in ninjas:
 #     print(ninja)
@@ -6,9 +19,9 @@ ninjas = ['ryu', 'crystal', 'yoshi','ken']
 # for ninja in ninjas[1:3]:
 #     print(ninja)
 
-for ninja in ninjas:
-    if ninja == "yoshi":
-        print(f'{ninja} - black belt' )
-        break
-    else:
-        print(ninja)
+# for ninja in ninjas:
+#     if ninja == "yoshi":
+#         print(f'{ninja} - black belt' )
+#         break
+#     else:
+#         print(ninja)
