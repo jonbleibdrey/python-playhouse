b02292cd02f473726c63ad3e00c29b225a9b9a62
jonathan bleibdrey <jonbleibdrey@aol.com>
2021-08-10 12:32:16 -0400

    added dictionaries

---
+++ lessons/dictionaries.py
@@ -0,0 +1,13 @@
+ninja_belts = {"crystal" : "red", "ryu": "black"}
+
+# to find all the keys in a varibale
+# dict_keys(['crystal', 'ryu'])
+# print(ninja_belts.keys())
+
+# dictionaries are basically objects in python!
+
+# to find if a key is actually in the dictionarie
+# print("ryu" in ninja_belts)
+
+# this will return a list of all the keys
+print(list(ninja_belts.keys()))
