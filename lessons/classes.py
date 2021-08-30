00df8af94a3f75f013d51509472e3f9bceb3eb77
jonathan bleibdrey <jonbleibdrey@aol.com>
2021-08-30 14:32:02 -0400

    added class and static methods

--- lessons/classes.py
+++ lessons/classes.py
@@ -1,5 +1,18 @@
 class Planet:
 
+    # class level attribute- has acesss to all instances
+    shape = "round"
+
+    #class methods
+    @classmethod
+    def commons(cls):
+        return f"All planets are {cls.shape} becuase of gravity"
+
+    #static methods
+    @staticmethod
+    def spin(speed = " 2000 miles per hour"):
+        return f"The planet spin and spins at {speed}"
+    #instint attribute- has acesss to the individual instants created
     def __init__(self, name, radius, gravity, system):
         self.name = name
         self.radius = radius
@@ -10,15 +23,14 @@ class Planet:
         return f"{self.name} is orbittin in the {self.system}"
 
 
-hoth = Planet("Hoth", 2000, 5.5, "hoth system")
-print(f"Name is : {hoth.name}")
-print(f"Radius is : {hoth.radius}")
-print(f"The gravity is : {hoth.gravity}")
-print(hoth.orbit())
+ 
 
 PlanetX = Planet("poopooo planet", 5000, 6.0, "big planet sytem")
-print(f"Name is : {PlanetX.name}")
-print(f"Radius is : {PlanetX.radius}")
-print(f"The gravity is : {PlanetX.gravity}")
-print(PlanetX.orbit())
+
+print(PlanetX.spin(1000))
+
+# print(f"Name is : {PlanetX.name}")
+# print(f"Radius is : {PlanetX.radius}")
+# print(f"The gravity is : {PlanetX.gravity}")
+# print(PlanetX.orbit())
 
