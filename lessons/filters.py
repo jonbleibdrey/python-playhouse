ea1ea7e297a6dfba6812216b39b5ec251c37c528
jonathan bleibdrey <jonbleibdrey@aol.com>
2021-08-31 13:45:10 -0400

    filter methods

---
+++ lessons/filters.py
@@ -0,0 +1,18 @@
+grades = ["A", "B", "C", "F", "D", "F", "A"]
+
+
+def remove_fails(grade):
+    return grade != "F"
+
+# print(list(filter(remove_fails, grades)))
+
+# filtered_grades = []
+
+# for grade in grades:
+#     if grade != "F":
+#         filtered_grades.append(grade)
+# print(filtered_grades)
+
+#comprehensive method
+
+print([grade for grade in grades if grade != "F"])
